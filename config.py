15d8001178cf3a2694a5a05df7b4a3d3d7a2671b
Francesca Sargent <ladyquas@francescas-mbp.home>
2015-10-22 18:22:22 +0100

    added pikaday to edit form

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ class Config:
 class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
-        'mysql://user:password@localhost/db'
+        'mysql://root:jeh5t@localhost/newlyn'
 
 
 class TestingConfig(Config):
