4eb2c26604e0142faed644d5435e357cdf6b98ee
Francesca Sargent <ladyquas@Francescas-MacBook-Pro.local>
2015-11-07 12:44:40 +0000

    Margins, back

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ class Config:
 class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
-        'mysql://root:sincynk1988@localhost/newlyn'
+        'mysql://root:jeh5t@localhost/newlyn'
 
 
 class TestingConfig(Config):
